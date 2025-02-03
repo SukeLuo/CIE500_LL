b80432e05fc6797d67c3cbed73f217dd1840bb51
SukeLuo <sukeluo27@gmail.com>
2025-02-02 21:59:54 -0500

    this is my 5th version of the file

--- week3/hello.py
+++ week3/hello.py
@@ -1,3 +1,18 @@
 print('Hello World')
 print('This is Linyue')
 print('haha')
+
+class car:
+    def __int__(self, color, model):
+        self.color = color
+        selfmodel = model
+    
+    def mymodel(self):
+        print(f'car model is {self.model}')
+    
+if __name__ == "__main__":
+    var1 = 0
+    a = car('black', 'mini')
+    b = car('white', 'nissan')
+    a.mymodel()
+    print(a.color, b.model)
